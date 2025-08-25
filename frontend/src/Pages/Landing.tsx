1382657fc3a5b1cf24ed8b2f6b3da16dea700b7a
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-08-25 16:20:04 +0600

    Frontend - Hero Section Completed

--- frontend/src/Pages/Landing.tsx
+++ frontend/src/Pages/Landing.tsx
@@ -2,7 +2,11 @@
 const Landing = () => {
   return (
     <div>
-        <h1>Minimal Library Management System</h1>
+        <section className="flex flex-col items-center justify-center h-[50vh] bg-yellow-100 mb-8 rounded-lg shadow">
+          <h1 className="text-5xl font-bold mb-4 text-orange-500">Minimal Library Management System</h1>
+          <p className="text-xl text-gray-700 mb-6">Effortlessly manage you library, books and borrowings.</p>
+          <button className="px-6 py-2 bg-orange-400 text-white font-semibold rounded hover:bg-orange-500 transition">Explre Books</button>
+        </section>
     </div>
   )
 }
